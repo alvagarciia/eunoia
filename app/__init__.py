88c7ddb70f2cc2f4ea8333fddedeb92c847c6a81
Alvaro Garcia <146588674+alvagarciia@users.noreply.github.com>
2025-07-26 15:11:06 -0500

    add python files

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+def create_app():
+    load_dotenv()  # Load secrets from .env
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "dev")
+
+    from .routes import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
